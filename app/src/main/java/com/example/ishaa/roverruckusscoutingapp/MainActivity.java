f8fa43ab91ecd2a16c3549570fcf35d767c0aa2d
RollerCoaster45 <ishaanoberoi@gmail.com>
2018-11-18 16:23:55 -0500

    Completed version one of application

--- app/src/main/java/com/example/ishaa/roverruckusscoutingapp/MainActivity.java
+++ app/src/main/java/com/example/ishaa/roverruckusscoutingapp/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.ishaa.roverruckusscoutingapp;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
@@ -15,12 +17,13 @@ import android.widget.Spinner;
 import android.widget.Switch;
 import android.widget.TextView;
 
+import org.apache.poi.hssf.record.formula.functions.Match;
 import org.apache.poi.ss.usermodel.Sheet;
 
 public class MainActivity extends AppCompatActivity {
     TextView AllianceLabel, BlueAllianceLabel, RedAllianceLabel, AutonomousLabel, CraterStartLabel, DepotStartLabel,
     SamplingLabel, TeleOpLabel, SilverLabelCargo, SilverCargoNumber, GoldCargoLabel, GoldCargoNumber, SilverDepotLabel,
-    SilverDepotNumber, GoldDepotLabel, GoldDepotNumber, EndGameLabel, EndPositionLabel, ScoreLabel, ScoreNumber;
+    SilverDepotNumber, GoldDepotLabel, GoldDepotNumber, EndGameLabel, EndPositionLabel, ScoreLabel, ScoreNumber, SamplingLocationLabel;
 
     Button RemoveSilverCargo, AddSilverCargo, RemoveGoldCargo, AddGoldCargo, RemoveSilverDepot, AddSilverDepot,
     RemoveGoldDepot, AddGoldDepot, Submit, Reset;
@@ -29,9 +32,11 @@ public class MainActivity extends AppCompatActivity {
 
     CheckBox AutoLand, AutoTeamMarker, AutoPark, FTAError;
 
-    Spinner SamplingSpinner, EndGameSpinner;
+    Spinner SamplingSpinner, EndGameSpinner, SamplingLocationSpinner;
 
     Switch AllianceSwitch, AutoStartSwitch;
+
+    DataLogger data;
 
     int score = 0;
     int silverCargoMinerals = 0;
@@ -50,6 +55,7 @@ public class MainActivity extends AppCompatActivity {
         CraterStartLabel = (TextView) findViewById(R.id.CraterStartLabel);
         DepotStartLabel = (TextView) findViewById(R.id.DepotStartLabel);
         SamplingLabel = (TextView) findViewById(R.id.SamplingLabel);
+
         TeleOpLabel = (TextView) findViewById(R.id.TeleOpLabel);
         SilverLabelCargo = (TextView) findViewById(R.id.SilverLabelCargo);
         SilverCargoNumber = (TextView) findViewById(R.id.SilverCargoNumber);
@@ -63,6 +69,7 @@ public class MainActivity extends AppCompatActivity {
         EndPositionLabel = (TextView) findViewById(R.id.EndPositionLabel);
         ScoreLabel = (TextView) findViewById(R.id.ScoreLabel);
         ScoreNumber = (TextView) findViewById(R.id.ScoreNumber);
+        SamplingLocationLabel = (TextView) findViewById(R.id.SamplingLocationLabel);
 
         RemoveSilverCargo = (Button) findViewById(R.id.RemoveSilverCargo);
         AddSilverCargo = (Button) findViewById(R.id.AddSilverCargo);
@@ -88,9 +95,10 @@ public class MainActivity extends AppCompatActivity {
         FTAError = (CheckBox) findViewById(R.id.FTAError);
 
         SamplingSpinner = (Spinner) findViewById(R.id.SamplingSpinner);
-        EndGameSpinner = (Spinner) findViewById(R.id.EndGameSpinner);
-
-
+        ArrayAdapter<CharSequence> samplingAdapter = ArrayAdapter.createFromResource(this,
+                R.array.SamplingSpinnerItems, android.R.layout.simple_spinner_item);
+        samplingAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        SamplingSpinner.setAdapter(samplingAdapter);
         SamplingSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
@@ -102,6 +110,12 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
+
+        EndGameSpinner = (Spinner) findViewById(R.id.EndGameSpinner);
+        ArrayAdapter<CharSequence> endGameAdapter = ArrayAdapter.createFromResource(this,
+                R.array.EndGameSpinner, android.R.layout.simple_spinner_item);
+        endGameAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        EndGameSpinner.setAdapter(endGameAdapter);
         EndGameSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
@@ -114,6 +128,22 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        SamplingLocationSpinner = (Spinner) findViewById(R.id.LocationSpinner);
+        ArrayAdapter<CharSequence> locationAdapter = ArrayAdapter.createFromResource(this,
+                R.array.SamplingLocation, android.R.layout.simple_spinner_item);
+        endGameAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        SamplingLocationSpinner.setAdapter(locationAdapter);
+        SamplingLocationSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                update();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
         AllianceSwitch = (Switch) findViewById(R.id.AllianceSwitch);
         AutoStartSwitch = (Switch) findViewById(R.id.StartingSwitch);
 
@@ -130,7 +160,7 @@ public class MainActivity extends AppCompatActivity {
                 GoldCargoNumber, SilverDepotLabel, SilverDepotNumber, GoldDepotLabel, GoldDepotNumber, RemoveSilverCargo, RemoveGoldCargo, RemoveSilverDepot, RemoveGoldDepot, AddSilverCargo,
                 AddGoldCargo, AddSilverDepot, AddGoldDepot};
 
-        View[] halfWidgets = {AllianceSwitch, AutoStartSwitch, Submit, Reset, ScoreLabel, ScoreNumber};
+        View[] halfWidgets = {AllianceSwitch, AutoStartSwitch, Submit, Reset, ScoreLabel, ScoreNumber, SamplingLocationSpinner, SamplingLocationLabel};
 
         View[] fullWidgets = {SheetName, TeamNumber, MatchNumber, AutonomousLabel, AllianceLabel, AutoLand, AutoTeamMarker, AutoPark, SamplingLabel, SamplingSpinner,
         TeleOpLabel, EndGameLabel, EndPositionLabel, EndGameSpinner, HangTime, FTAError, AdditionalComments, Initials};
@@ -154,7 +184,129 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
-    public void submit() {
+    public void submit(View view) {
+        if(TeamNumber.getText().toString().trim().equals("")){
+            AlertDialog.Builder builder1 = new AlertDialog.Builder(this);
+            builder1.setMessage("No Team Number");
+            builder1.setCancelable(true);
+            builder1.setPositiveButton(
+                    "Cancel",
+                    new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int id) {
+                            dialog.cancel();
+                        }
+                    });
+            AlertDialog alert11 = builder1.create();
+            alert11.show();
+        }else if(MatchNumber.getText().toString().trim().equals("")){
+            AlertDialog.Builder builder1 = new AlertDialog.Builder(this);
+            builder1.setMessage("No match Number");
+            builder1.setCancelable(true);
+            builder1.setPositiveButton(
+                    "Cancel",
+                    new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int id) {
+                            dialog.cancel();
+                        }
+                    });
+            AlertDialog alert11 = builder1.create();
+            alert11.show();
+        }else if(SheetName.getText().toString().trim().equals("")){
+            AlertDialog.Builder builder1 = new AlertDialog.Builder(this);
+            builder1.setMessage("No Sheet Name");
+            builder1.setCancelable(true);
+            builder1.setPositiveButton(
+                    "Cancel",
+                    new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int id) {
+                            dialog.cancel();
+                        }
+                    });
+            AlertDialog alert11 = builder1.create();
+            alert11.show();
+        }else if((EndGameSpinner.getSelectedItem().equals("Hung")&&HangTime.getText().toString().equals(""))||(!EndGameSpinner.getSelectedItem().equals("Hung")&&!HangTime.getText().toString().equals(""))){
+            AlertDialog.Builder builder1 = new AlertDialog.Builder(this);
+            builder1.setMessage("Can't have a hang without time or time without hang");
+            builder1.setCancelable(true);
+            builder1.setPositiveButton(
+                    "Cancel",
+                    new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int id) {
+                            dialog.cancel();
+                        }
+                    });
+            AlertDialog alert11 = builder1.create();
+            alert11.show();
+        }else if(!(EndGameSpinner.getSelectedItem().equals("hung"))&&(Integer.parseInt(HangTime.getText().toString())>30)) {
+            AlertDialog.Builder builder1 = new AlertDialog.Builder(this);
+            builder1.setMessage("Hang seconds too large");
+            builder1.setCancelable(true);
+            builder1.setPositiveButton(
+                    "Cancel",
+                    new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int id) {
+                            dialog.cancel();
+                        }
+                    });
+            AlertDialog alert11 = builder1.create();
+            alert11.show();
+        }else{
+            AlertDialog.Builder builder1 = new AlertDialog.Builder(this);
+            builder1.setMessage("Are you sure you would like to submit?");
+            builder1.setCancelable(true);
+            builder1.setNegativeButton(
+                    "Cancel",
+                    new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int id) {
+                            dialog.cancel();
+                        }
+                    });
+            builder1.setPositiveButton(
+                    "Submit",
+                    new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int id) {
+                            dataSubmit();
+                            dialog.cancel();
+                        }
+                    });
+            AlertDialog alert11 = builder1.create();
+            alert11.show();
+        }
+    }
+
+    public void dataSubmit(){
+        data = new DataLogger(SheetName.getText().toString() + ".xls");
+        data.resetAndNextRow(this);
+        data.addField(Integer.parseInt(TeamNumber.getText().toString()));
+        data.addField(Integer.parseInt(MatchNumber.getText().toString()));
+        if (AllianceSwitch.isChecked()) {
+            data.addField("Blue");
+        } else {
+            data.addField("red");
+        }
+        if(AutoStartSwitch.isChecked()){
+            data.addField("Depot");
+        }else{
+            data.addField("Crater");
+        }
+        data.addField(AutoLand.isChecked());
+        data.addField(AutoTeamMarker.isChecked());
+        data.addField(AutoPark.isChecked());
+        data.addField((String) SamplingSpinner.getSelectedItem());
+        data.addField((String) SamplingLocationSpinner.getSelectedItem());
+        data.addField(silverCargoMinerals);
+        data.addField(goldCargoMinerals);
+        data.addField(silverDepotMinerals);
+        data.addField(goldDepotMinerals);
+        data.addField((String) EndGameSpinner.getSelectedItem());
+        data.addField(HangTime.getText().toString());
+        data.addField(score);
+        data.addField(FTAError.isChecked());
+        data.addField(AdditionalComments.getText().toString());
+        data.addField(Initials.getText().toString());
+        data.newLine();
+        data.saveDataLogger(this);
+        reset();
 
     }
 
@@ -168,7 +320,22 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void reset(){
-
+        TeamNumber.setText("");
+        MatchNumber.setText("");
+        AutoLand.setChecked(false);
+        AutoTeamMarker.setChecked(false);
+        AutoPark.setChecked(false);
+        SamplingSpinner.setSelection(0);
+        SamplingLocationSpinner.setSelection(0);
+        silverCargoMinerals = 0;
+        goldCargoMinerals = 0;
+        silverDepotMinerals = 0;
+        goldDepotMinerals = 0;
+        EndGameSpinner.setSelection(0);
+        HangTime.setText("");
+        FTAError.setChecked(false);
+        score = 0;
+        AdditionalComments.setText("");
     }
 
     public void addSilverCargo(View view){
@@ -193,6 +360,7 @@ public class MainActivity extends AppCompatActivity {
     }
     public void addSilverDepot(View view){
         silverDepotMinerals++;
+        update();
     }
     public void subSilverDepot(View view){
         if(silverDepotMinerals>0){
@@ -206,12 +374,34 @@ public class MainActivity extends AppCompatActivity {
     }
     public void subGoldDepot(View view){
         if(goldDepotMinerals>0){
-            goldDepotMinerals++;
+            goldDepotMinerals--;
             update();
         }
     }
     public void updateClick(View view){
         update();
+    }
+    public void resetButtonClick(View view){
+        AlertDialog.Builder builder1 = new AlertDialog.Builder(this);
+        builder1.setMessage("Are you sure you would like to reset?");
+        builder1.setCancelable(true);
+        builder1.setNegativeButton(
+                "Cancel",
+                new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        dialog.cancel();
+                    }
+                });
+        builder1.setPositiveButton(
+                "Reset",
+                new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        reset();
+                        dialog.cancel();
+                    }
+                });
+        AlertDialog alert11 = builder1.create();
+        alert11.show();
     }
     public void calculateScore(){
         score=0;
@@ -226,5 +416,17 @@ public class MainActivity extends AppCompatActivity {
         }
         score+=(goldCargoMinerals+silverCargoMinerals)*5;
         score+=(goldDepotMinerals+silverDepotMinerals)*2;
+        if(SamplingSpinner.getSelectedItem().equals("1 Knocked Correctly")){
+            score+=25;
+        }else if(SamplingSpinner.getSelectedItem().equals("2 Knocked Correctly")){
+            score += 50;
+        }
+        if(EndGameSpinner.getSelectedItem().equals("Partially Parked")){
+            score+=10;
+        }else if(EndGameSpinner.getSelectedItem().equals("Fully Parked")){
+            score+= 25;
+        }else if(EndGameSpinner.getSelectedItem().equals("Hung")){
+            score+= 50;
+        }
     }
 }
